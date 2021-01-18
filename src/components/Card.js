71d19c75dad6585ead47413cb82378bccf6dc044
Sachin Verma <sachinverma53121@gmail.com>
2021-01-19 00:35:37 +0530

    users followers details

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
+import React from "react";
+import styled from "styled-components";
+import { MdBusiness, MdLocationOn, MdLink } from "react-icons/md";
+
+import { GithubContext } from "../context/context";
+
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username,
+  } = githubUser;
+
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || "twitter n/a"}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </header>
+      <p className="bio">{bio || "no bio available"}</p>
+      <div className="links">
+        <p>
+          <MdBusiness /> {company || "company n/a"}
+        </p>
+        <p>
+          <MdLocationOn /> {location || "location n/a"}
+        </p>
+        {blog ? (
+          <a href={`https://${blog}`}>
+            <MdLink /> {blog}
+          </a>
+        ) : (
+          <p>no blogs link</p>
+        )}
+      </div>
+    </Wrapper>
+  );
 };
+
 const Wrapper = styled.article`
   background: var(--clr-white);
   padding: 1.5rem 2rem;
@@ -13,7 +55,7 @@ const Wrapper = styled.article`
   border-bottom-right-radius: var(--radius);
   position: relative;
   &::before {
-    content: 'user';
+    content: "user";
     position: absolute;
     top: 0;
     left: 0;
--- src/context/context.js
+++ src/context/context.js
@@ -12,8 +12,14 @@ const GithubContext = React.createContext();
 // Provider, Consumer - GithubContext.Provider
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setGithubFollowers] = useState(mockFollowers);
+
   return (
-    <GithubContext.Provider value={"hello"}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 
